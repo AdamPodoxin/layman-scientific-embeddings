537e0aacf188552ec69e52a7446e5ac32e6999bc
alp11 <alp11@sfu.ca>
2026-03-18 17:45:58 -0700

    Train vanilla finetuned model on csil

--- train-vanilla-model.py
+++ train-vanilla-model.py
@@ -6,7 +6,7 @@ from sentence_transformers import (
         losses, 
     )
 from sentence_transformers.training_args import BatchSamplers
-from datasets import Dataset, load_from_disk, concatenate_datasets
+from datasets import Dataset, DatasetDict, load_from_disk, concatenate_datasets
 
 
 BASE_EMBEDDING_MODEL = "allenai/scibert_scivocab_uncased"
@@ -25,7 +25,6 @@ JARGON_LAYMAN_PAIRS_PATH = DATA_PATH / "pairs" / "jargon-layman"
 
 MODELS_PATH = Path("models")
 VANILLA_FINETUNED_MODEL_PATH = MODELS_PATH / "vanilla-finetuned"
-JARGON_LAYMAN_FINETUNED_MODEL_PATH = MODELS_PATH / "jargon-layman-finetuned"
 
 # All combinations of jargon-jargon, layman-layman, and jargon-layman,
 # as well as abstract-jargon, abstract-layman, title-jargon, and title-layman. 
@@ -35,67 +34,109 @@ MINI_BATCH_SIZE = NUM_PAIRS_PER_ABSTRACT * NUM_ABSTRACTS_IN_BATCH
 
 LEARNING_RATE = 1e-5
 WEIGHT_DECAY = 1e-4
+BATCH_SIZE = 30
 
-PROP_PAIRS_TO_TAKE = 0.20
+PROP_PAIRS_TO_TAKE = 0.25
 
 
 def main():
-    abstract_jargon_pairs_dataset: Dataset = load_from_disk(str(ABSTRACT_JARGON_PAIRS_PATH))["train"]
-    abstract_layman_pairs_dataset: Dataset = load_from_disk(str(ABSTRACT_LAYMAN_PAIRS_PATH))["train"]
+    abstract_jargon_pairs_dataset: DatasetDict = load_from_disk(str(ABSTRACT_JARGON_PAIRS_PATH))
+    abstract_layman_pairs_dataset: DatasetDict = load_from_disk(str(ABSTRACT_LAYMAN_PAIRS_PATH))
     
-    title_jargon_pairs_dataset: Dataset = load_from_disk(str(TITLE_JARGON_PAIRS_PATH))["train"]
-    title_layman_pairs_dataset: Dataset = load_from_disk(str(TITLE_LAYMAN_PAIRS_PATH))["train"]
+    title_jargon_pairs_dataset: DatasetDict = load_from_disk(str(TITLE_JARGON_PAIRS_PATH))
+    title_layman_pairs_dataset: DatasetDict = load_from_disk(str(TITLE_LAYMAN_PAIRS_PATH))
 
-    jargon_jargon_pairs_dataset: Dataset = load_from_disk(str(JARGON_JARGON_PAIRS_PATH))["train"]
-    layman_layman_pairs_dataset: Dataset = load_from_disk(str(LAYMAN_LAYMAN_PAIRS_PATH))["train"]
-    jargon_layman_pairs_dataset: Dataset = load_from_disk(str(JARGON_LAYMAN_PAIRS_PATH))["train"]
+    jargon_jargon_pairs_dataset: DatasetDict = load_from_disk(str(JARGON_JARGON_PAIRS_PATH))
+    layman_layman_pairs_dataset: DatasetDict = load_from_disk(str(LAYMAN_LAYMAN_PAIRS_PATH))
+    jargon_layman_pairs_dataset: DatasetDict = load_from_disk(str(JARGON_LAYMAN_PAIRS_PATH))
 
-    keyword_keyword_pairs_dataset = concatenate_datasets([
-        jargon_jargon_pairs_dataset,
-        layman_layman_pairs_dataset,
-        jargon_layman_pairs_dataset,
+    keyword_keyword_pairs_dataset_train = concatenate_datasets([
+        jargon_jargon_pairs_dataset["train"],
+        layman_layman_pairs_dataset["train"],
+        jargon_layman_pairs_dataset["train"],
     ])
 
-    abstract_keyword_pairs_dataset = concatenate_datasets([
-        abstract_jargon_pairs_dataset,
-        abstract_layman_pairs_dataset,
+    abstract_keyword_pairs_dataset_train = concatenate_datasets([
+        abstract_jargon_pairs_dataset["train"],
+        abstract_layman_pairs_dataset["train"],
     ])
 
-    title_keyword_pairs_dataset = concatenate_datasets([
-        title_jargon_pairs_dataset,
-        title_layman_pairs_dataset,
+    title_keyword_pairs_dataset_train = concatenate_datasets([
+        title_jargon_pairs_dataset["train"],
+        title_layman_pairs_dataset["train"],
     ])
 
-    full_dataset = concatenate_datasets([
-        keyword_keyword_pairs_dataset,
-        abstract_keyword_pairs_dataset,
-        title_keyword_pairs_dataset,
+    full_dataset_train = concatenate_datasets([
+        keyword_keyword_pairs_dataset_train,
+        abstract_keyword_pairs_dataset_train,
+        title_keyword_pairs_dataset_train,
     ])
 
     # For efficiency, taking a subset of the entire pairs dataset
-    train_dataset = full_dataset \
+    train_dataset = full_dataset_train \
                                 .shuffle() \
-                                .take(int(PROP_PAIRS_TO_TAKE * full_dataset.shape[0]))
+                                .take(int(PROP_PAIRS_TO_TAKE * full_dataset_train.shape[0]))
 
-    vanilla_finetuned_model = SentenceTransformer(BASE_EMBEDDING_MODEL)
+    keyword_keyword_pairs_dataset_val = concatenate_datasets([
+        jargon_jargon_pairs_dataset["val"],
+        layman_layman_pairs_dataset["val"],
+        jargon_layman_pairs_dataset["val"],
+    ])
 
-    loss = losses.CachedMultipleNegativesRankingLoss(vanilla_finetuned_model, mini_batch_size=MINI_BATCH_SIZE)
+    abstract_keyword_pairs_dataset_val = concatenate_datasets([
+        abstract_jargon_pairs_dataset["val"],
+        abstract_layman_pairs_dataset["val"],
+    ])
+
+    title_keyword_pairs_dataset_val = concatenate_datasets([
+        title_jargon_pairs_dataset["val"],
+        title_layman_pairs_dataset["val"],
+    ])
+
+    full_dataset_val = concatenate_datasets([
+        keyword_keyword_pairs_dataset_val,
+        abstract_keyword_pairs_dataset_val,
+        title_keyword_pairs_dataset_val,
+    ])
+
+    # For efficiency, taking a subset of the entire pairs dataset
+    val_dataset = full_dataset_val \
+                                .shuffle() \
+                                .take(int(PROP_PAIRS_TO_TAKE * full_dataset_val.shape[0]))
+
+    model = SentenceTransformer(BASE_EMBEDDING_MODEL)
+
+    loss = losses.CachedMultipleNegativesRankingLoss(model, mini_batch_size=MINI_BATCH_SIZE)
 
     args = SentenceTransformerTrainingArguments(
         output_dir=VANILLA_FINETUNED_MODEL_PATH,
+
         learning_rate=LEARNING_RATE,
         weight_decay=WEIGHT_DECAY,
         batch_sampler=BatchSamplers.NO_DUPLICATES,
+
+        eval_strategy="epoch",
+        save_strategy="epoch",
+        load_best_model_at_end=True,
+        metric_for_best_model="eval_loss",
+        save_total_limit=1,
+        save_only_model=True,
+
+        per_device_train_batch_size=BATCH_SIZE,
+        per_device_eval_batch_size=BATCH_SIZE,
     )
 
     trainer = SentenceTransformerTrainer(
-        model=vanilla_finetuned_model,
+        model=model,
         train_dataset=train_dataset,
+        eval_dataset=val_dataset,
         loss=loss,
         args=args,
     )
 
     trainer.train()
+
+    trainer.save_model()
 
 
 if __name__ == "__main__":
